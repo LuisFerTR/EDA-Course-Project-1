40f19baaf396f8b5e6673cea760bbd9d33b4e135
Luis Talavera <luisfertalavera15@gmail.com>
2022-07-11 20:43:20 -0500

    All figure replicated

---
+++ plot2.R
@@ -0,0 +1,25 @@
+library(dplyr) # mutate
+
+# Load data
+data <- read.table("household_power_consumption.txt", stringsAsFactors = FALSE,
+                   skip = 66637, nrows = 2880, na.strings = "?", sep = ";")
+
+# Assign variables name
+variables <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage",
+               "Global_intensity","Sub_metering_1", "Sub_metering_2","Sub_metering_3")
+names(data) <- variables
+
+date.time <- paste(data$Date, data$Time)
+
+data <- mutate(data, Date = as.Date(Date, format = "%d/%m/%Y"), 
+               Time = strptime(date.time, "%d/%m/%Y %H:%M:%S"))
+
+# Open Grpahic file device
+png(filename = "plot2.png")
+
+with(data, plot(y=Global_active_power, x=Time, 
+                type = "l", 
+                ylab = "Global Active Power (kilowatts)"))
+                
+# Close Grpahic file device
+dev.off()
